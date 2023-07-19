3303e59b0583b55fec7ef59ec5bf3a2ef4d0531b
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2023-07-19 17:33:11 +0530

    swapTokensAndstakeDev initial

---
+++ contracts/SwapTokensAndStakeDev.sol
@@ -0,0 +1,216 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity =0.8.7;
+pragma abicoder v2;
+
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import {ILockup} from "./interfaces/ILockup.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./Escrow.sol";
+
+
+contract SwapTokensAndStakeDev is Escrow {
+    // solhint-disable-next-line const-name-snakecase
+	ISwapRouter public constant uniswapRouter =
+		ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+	// solhint-disable-next-line const-name-snakecase
+	IQuoter public constant quoter =
+		IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
+
+    address public devAddress;
+	address public lockupAddress;
+	address public sTokensAddress;
+
+    
+    struct Amounts {
+		uint256 input;
+		uint256 fee;
+	}
+	mapping(address => Amounts) public gatewayOf;
+
+    constructor(
+		address _devAddress,
+		address _lockupAddress,
+		address _sTokensAddress
+	) {
+		devAddress = _devAddress;
+		lockupAddress = _lockupAddress;
+		sTokensAddress = _sTokensAddress;
+	}
+
+	// For native token
+	function swapTokensAndStakeDev(
+		bytes memory path,
+		address property,
+		uint256 _amountOut,
+		uint256 deadline,
+		bytes32 payload,
+		address payable gatewayAddress,
+		uint256 gatewayFee
+	) external payable {
+		require(msg.value > 0, "Must pass non-zero amount");
+		require(gatewayFee <= 10000, "must be below 10000");
+		// handle fee
+		uint256 feeAmount = (msg.value * gatewayFee) / 10000;
+		_deposit(gatewayAddress, feeAmount, address(0));
+
+		gatewayOf[gatewayAddress] = Amounts(msg.value, feeAmount);
+
+		_swapTokensAndStakeDev(
+			path,
+			property,
+			(msg.value - feeAmount),
+			_amountOut,
+			deadline,
+			payload
+		);
+		delete gatewayOf[gatewayAddress];
+	}
+
+	// For ERC20
+	function swapTokensAndStakeDev(
+		IERC20 token,
+		bytes memory path,
+		address property,
+		uint256 amount,
+		uint256 _amountOut,
+		uint256 deadline,
+		bytes32 payload,		
+		address payable gatewayAddress,
+		uint256 gatewayFee
+	) external {
+		require(gatewayFee <= 10000, "must be below 10000");
+		require(
+			token.allowance(msg.sender, address(this)) >= amount,
+			"insufficient allowance"
+		);
+		// Transfer the amount from the user to the contract
+		TransferHelper.safeTransferFrom(
+			address(token),
+			msg.sender,
+			address(this),
+			amount
+		);
+		// handle fee
+		uint256 feeAmount = (amount * gatewayFee) / 10000;
+		_deposit(gatewayAddress, feeAmount, address(token));
+
+		gatewayOf[gatewayAddress] = Amounts(amount, feeAmount);
+
+		_swapTokensAndStakeDev(
+			token,
+			path,
+			property,
+			(amount - feeAmount),
+			_amountOut,
+			deadline,
+			payload
+		);
+		delete gatewayOf[gatewayAddress];
+	}
+	
+	// do not use on-chain, gas inefficient!
+	function getEstimatedTokensForDev(uint256 devAmount, bytes memory path)
+		external
+		returns (uint256)
+	{
+		return
+			quoter.quoteExactInput(path, devAmount);
+	}
+
+	// do not use on-chain, gas inefficient!
+	function getEstimatedDevForTokens(
+		uint256 tokenAmount,
+		bytes memory path
+	) external returns (uint256) {
+		return
+		quoter.quoteExactInput(path, tokenAmount);
+	}
+
+	// erc20
+	function _swapTokensAndStakeDev(
+		IERC20 token,
+		bytes memory path,
+		address property,
+		uint256 amount,
+		uint256 _amountOut,
+		uint256 deadline,
+		bytes32 payload
+	) private {
+
+		address recipient = address(this);
+		uint256 amountIn = amount;
+
+		// 	Approve the router to spend the token amount
+		TransferHelper.safeApprove(
+			address(token),
+			address(uniswapRouter),
+			amount
+		);
+		
+		// Multiple pool swaps are encoded through bytes called a `path`. A path is a sequence of token addresses and poolFees that define the pools used in the swaps.
+		// The format for pool encoding is (tokenIn, fee, tokenOut/tokenIn, fee, tokenOut) where tokenIn/tokenOut parameter is the shared token across the pools.
+		ISwapRouter.ExactInputParams memory params = ISwapRouter
+			.ExactInputParams({
+				path: path,
+				recipient: recipient,
+				deadline: deadline,
+				amountIn: amountIn,
+				amountOutMinimum: _amountOut
+			});
+		
+		uint256 amountOut = uniswapRouter.exactInput(params);
+		IERC20(devAddress).approve(lockupAddress, amountOut);
+		uint256 tokenId = ILockup(lockupAddress).depositToProperty(
+			property,
+			amountOut,
+			payload
+		);
+		IERC721(sTokensAddress).safeTransferFrom(
+			address(this),
+			msg.sender,
+			tokenId
+		);
+	}
+
+	// native token
+	function _swapTokensAndStakeDev(
+		bytes memory path,
+		address property,
+		uint256 amount,
+		uint256 _amountOut,
+		uint256 deadline,
+		bytes32 payload
+	) private {
+		address recipient = address(this);
+		uint256 amountIn = amount;
+
+		// Multiple pool swaps are encoded through bytes called a `path`. A path is a sequence of token addresses and poolFees that define the pools used in the swaps.
+		// The format for pool encoding is (tokenIn, fee, tokenOut/tokenIn, fee, tokenOut) where tokenIn/tokenOut parameter is the shared token across the pools.
+		ISwapRouter.ExactInputParams memory params = ISwapRouter
+			.ExactInputParams({
+				path: path,
+				recipient: recipient,
+				deadline: deadline,
+				amountIn: amountIn,
+				amountOutMinimum: _amountOut
+			});
+		uint256 amountOut = uniswapRouter.exactInput{value:amount}(
+			params
+		);
+		
+		IERC20(devAddress).approve(lockupAddress, amountOut);
+		uint256 tokenId = ILockup(lockupAddress).depositToProperty(
+			property,
+			amountOut,
+			payload
+		);
+		IERC721(sTokensAddress).safeTransferFrom(
+			address(this),
+			msg.sender,
+			tokenId
+		);
+	}
+}
---
+++ test/swap-tokens-stake-dev.ts
@@ -0,0 +1,191 @@
+import { expect, use } from 'chai'
+import { solidity } from 'ethereum-waffle'
+import { ethers, waffle } from 'hardhat'
+import { SwapTokensAndStakeDev, ISwapRouter } from '../typechain'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { Contract, BigNumber } from 'ethers'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const alchemyKeyPolygon =
+	typeof process.env.ALCHEMY_KEY_POLYGON === 'undefined'
+		? ''
+		: process.env.ALCHEMY_KEY_POLYGON
+
+use(solidity)
+
+type Path = {
+    token1: string
+    fee1: BigNumber
+    token2: string
+    fee2: BigNumber
+    token3: string
+}
+
+describe('SwapTokensAndStakeDev', () => {
+    let account1: SignerWithAddress
+	let gateway: SignerWithAddress
+    let swapTokensAndStakeContract: SwapTokensAndStakeDev
+    let lockupContract: Contract
+    let sTokensManagerContract: Contract
+
+
+    // Polygon
+	const usdcAddress = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+	const wethAddress = '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
+	const weth9 = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
+	const devAddress = '0xA5577D1cec2583058A6Bd6d5DEAC44797c205701'
+	const lockupAddress = '0x42767B12d3f07bE0D951a64eE6573B40Ff165C4e'
+	const propertyAddress = '0x803854e0676cd5892f6100eb452551D22e9c38ec'
+	const sTokensManagerAddress = '0x89904De861CDEd2567695271A511B3556659FfA2'
+	const SwapRouterAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564'
+	let usdcContract: Contract
+	let swapRouter: ISwapRouter
+
+    beforeEach(async () => {
+        await ethers.provider.send('hardhat_reset', [
+            {
+                forking: {
+                    jsonRpcUrl:
+                        'https://polygon-mainnet.g.alchemy.com/v2/' + alchemyKeyPolygon,
+                    blockNumber: 45237517,
+                },
+            },
+        ])
+
+        const accounts = await ethers.getSigners()
+		account1 = accounts[0]
+		gateway = accounts[1]
+
+        const factory = await ethers.getContractFactory('SwapTokensAndStakeDev')
+        swapTokensAndStakeContract = (await factory.deploy(
+            devAddress,
+            lockupAddress,
+            sTokensManagerAddress
+        )) as SwapTokensAndStakeDev
+
+        await swapTokensAndStakeContract.deployed()
+
+        usdcContract = new ethers.Contract(
+			usdcAddress,
+			[
+				'function balanceOf(address owner) view returns (uint256)',
+				'function approve(address spender, uint256 amount) public returns (bool)',
+				'function transfer(address to, uint256 amount) external returns (bool)',
+			],
+			ethers.provider
+		)
+		swapRouter = (await ethers.getContractAt(
+			'@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol:ISwapRouter',
+			SwapRouterAddress
+		)) as ISwapRouter
+
+		lockupContract = await ethers.getContractAt(
+			'contracts/interfaces/ILockup.sol:ILockup',
+			lockupAddress
+		)
+		sTokensManagerContract = await ethers.getContractAt(
+			'contracts/interfaces/ISTokensManager.sol:ISTokensManager',
+			sTokensManagerAddress
+		)
+    })
+    describe('SwapTokensAndStakeDev', () => {
+        it('should swap tokens and stake dev', async () => {
+            // Get latest block
+			const block = await waffle.provider.getBlock('latest')
+			const deadline = block.timestamp + 300
+
+            // Get USDC via MATIC(Native Token) -> wMATIC -> USDC
+			await swapRouter.connect(account1).exactInputSingle(
+				{
+					tokenIn: weth9,
+					tokenOut: usdcAddress,
+					fee: 3000,
+					recipient: account1.address,
+					deadline,
+					amountIn: ethers.utils.parseEther('1700'),
+					amountOutMinimum: 0,
+					sqrtPriceLimitX96: 0,
+				},
+				{
+					value: ethers.utils.parseEther('1700'),
+				}
+			)
+
+            // Approve USDC
+            await usdcContract.connect(account1).approve(
+                swapTokensAndStakeContract.address,
+                ethers.utils.parseUnits('1900', 6)
+            )
+
+            const path:Path = 
+            {
+                token1: usdcAddress,
+                fee1: BigNumber.from(10000),
+                token2: wethAddress,
+                fee2: BigNumber.from(10000),
+                token3: devAddress,
+            }
+
+            // Use callStaic to execute getEstimatedDevForUsdc as a read method
+			const amountOut =
+            await swapTokensAndStakeContract.callStatic.getEstimatedDevForTokens(
+                ethers.utils.parseUnits('1', 6),
+                ethers.utils.solidityPack(
+                    ['address', 'uint24', 'address', 'uint24', 'address'],
+                    [path.token1, path.fee1, path.token2, path.fee2, path.token3]
+                )
+            )
+            console.log('amountOut', amountOut.toString())
+            const amountIn =
+            await swapTokensAndStakeContract.callStatic.getEstimatedTokensForDev(
+                amountOut,
+                ethers.utils.solidityPack(
+                    ['address', 'uint24', 'address', 'uint24', 'address'],
+                    [path.token3, path.fee2, path.token2, path.fee1, path.token1]
+                )
+            )
+            console.log('amountIn', amountIn.toString())
+            // Assuming only 1% slippage, it can be dynamic so need to make more better assertion
+            const expected = ethers.utils.parseUnits('1', 6)
+            expect(amountIn).to.lte(expected.sub(expected.mul(1).div(100)))
+            // STokenId = currentIndex + 1 will be minted.
+			let sTokenId: BigNumber = await sTokensManagerContract.currentIndex()
+			sTokenId = sTokenId.add(1)
+
+			await expect(
+				await swapTokensAndStakeContract
+					.connect(account1)
+					['swapTokensAndStakeDev(address,bytes,address,uint256,uint256,uint256,bytes32,address,uint256)'](
+                        usdcAddress,
+                        ethers.utils.solidityPack(
+                            ['address', 'uint24', 'address', 'uint24', 'address'],
+                            [path.token1, path.fee1, path.token2, path.fee2, path.token3]
+                        ),
+                        propertyAddress,
+                        ethers.utils.parseUnits('900', 6),
+                        amountOut,
+                        deadline,
+                        ethers.utils.formatBytes32String(''),
+                        gateway.address,
+                        10000,
+					)
+			)
+				.to.emit(lockupContract, 'Lockedup')
+				.withArgs(
+					swapTokensAndStakeContract.address,
+					propertyAddress,
+					amountOut,
+					sTokenId
+				)
+
+			const sTokenOwner = await sTokensManagerContract.ownerOf(sTokenId)
+			const sTokenPosition: number[] = await sTokensManagerContract.positions(
+				sTokenId
+			)
+			expect(sTokenOwner).to.equal(account1.address)
+			expect(sTokenPosition[1]).to.equal(amountOut)
+        })
+    })
+})
