8cde8ef7d83e012aae60f030789930ba894ae026
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-06-25 16:38:21 +0530

    add: logic for ecosystem fee

--- contracts/SwapTokensAndStakeDev.sol
+++ contracts/SwapTokensAndStakeDev.sol
@@ -149,8 +149,16 @@ contract SwapTokensAndStakeDev is Escrow, Initializable {
 		require(msg.value > 0, "Must pass non-zero amount");
 		require(_gatewayFee <= 10000, "must be below or equal 10000");
 		// handle fee
+		uint256 ecosystemFeeAmount = _gatewayFee >= ecosystemFeeThreshold
+			? (msg.value * ecosystemFee) / 10000
+			: 0;
+
 		uint256 feeAmount = (msg.value * _gatewayFee) / 10000;
-		_deposit(_gatewayAddress, feeAmount, address(0));
+
+		uint256 acutualFeeAmount = ((msg.value - ecosystemFeeAmount) * _gatewayFee) / 10000;
+
+		_deposit(owner, ecosystemFeeAmount, address(0));
+		_deposit(_gatewayAddress, acutualFeeAmount, address(0));
 
 		gatewayOf[_gatewayAddress] = Amounts(address(0), msg.value, feeAmount);
 
@@ -158,7 +166,7 @@ contract SwapTokensAndStakeDev is Escrow, Initializable {
 			_to,
 			_path,
 			_property,
-			(msg.value - feeAmount),
+			(msg.value - ecosystemFeeAmount - acutualFeeAmount),
 			_amountOut,
 			_deadline,
 			_payload
@@ -256,9 +264,19 @@ contract SwapTokensAndStakeDev is Escrow, Initializable {
 			address(this),
 			_amount
 		);
+
+		uint256 ecosystemFeeAmount = _gatewayFee >= ecosystemFeeThreshold
+			? (_amount * ecosystemFee) / 10000
+			: 0;
+
 		// handle fee
 		uint256 feeAmount = (_amount * _gatewayFee) / 10000;
-		_deposit(_gatewayAddress, feeAmount, address(_token));
+		// handle acutual gateway fee
+		uint256 acutualFeeAmount = ((_amount - ecosystemFeeAmount) *
+			_gatewayFee) / 10000;
+		
+		_deposit(owner, ecosystemFeeAmount, address(_token));
+		_deposit(_gatewayAddress, acutualFeeAmount, address(_token));
 
 		gatewayOf[_gatewayAddress] = Amounts(
 			address(_token),
@@ -271,7 +289,7 @@ contract SwapTokensAndStakeDev is Escrow, Initializable {
 			_token,
 			_path,
 			_property,
-			(_amount - feeAmount),
+			(_amount - ecosystemFeeAmount - acutualFeeAmount),
 			_amountOut,
 			_deadline,
 			_payload
